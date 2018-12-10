65f348fff44f17b360d2936a79190609295a38e8
xuefeicheng <xuefeicheng_ytu@163.com>
2018-12-10 18:09:02 +0800

    引入vux nprogress

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+// import NProgress from 'nprogress' // Progress 进度条
+// import 'nprogress/nprogress.css'// Progress 进度条样式
 import LayOut from '@/components/layout/layOut'
 import helloWorld from '@/components/HelloWorld'
 
@@ -23,8 +25,21 @@ const initialRouteMap = [
     ]
   }
 ]
+// 简单配置
+// NProgress.inc(0.2)
+// NProgress.configure({ easing: 'ease', speed: 50000, showSpinner: false })
 const router = new Router({
   routes: initialRouteMap
 })
 
+// 全局路由开始守卫
+// router.beforeEach((to, from, next) => {
+//   NProgress.start()
+//   NProgress.done()
+// })
+
+// 全局路由结束守卫
+// router.afterEach(() => {
+//   NProgress.done()
+// })
 export default router
